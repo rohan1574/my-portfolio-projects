b929728776dd78e0df07cab81b5cba536a9402a1
Rony <mrrohan192@gmail.com>
2024-10-14 00:26:34 +0600

    the added that banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,7 +4,7 @@ import RightBanner from './RightBanner';
 const Banner = () => {
   return (
     <section
-      id="homes"
+      id="home"
       className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
     >
       <LeftBanner />
